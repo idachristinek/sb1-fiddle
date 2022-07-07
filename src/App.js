41942231c0e415b6995f9a82eea9905d3e7aed2b
Karianne Kristiansen <karianne.kristiansen@sparebank1.no>
2022-07-07 14:25:59 +0200

    Add Sparebank 1 logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Navigation from "./components/navigation";
+import logo from "./logo.svg";
 import {ActionButton} from "@sb1/ffe-buttons-react";
 
 function App() {
   return (
     <div className="App">
+      <img src={logo} alt="Sparebank 1" className="logo"/>
     <Navigation/>
 
       <header className="App-header">
