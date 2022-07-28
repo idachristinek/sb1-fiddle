421bc5e8927231a044a7ad383a672655385d9c18
Karianne Kristiansen <karianne.kristiansen@sparebank1.no>
2022-07-28 10:48:05 +0200

    Add icon card and an example

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,11 +1,14 @@
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
+import logo from "../logo.svg";
 
 function Navigation() {
     return (
         <div className="Navbar">
+            <img src={logo} alt="Sparebank 1" className="logo"/>
             <nav>
                 <Link to="/">App</Link> |{" "}
-                <Link to="/2">Page2</Link>
+                <Link to="/2">Page2</Link> |{" "}
+                <Link to="/3">Page3</Link>
             </nav>
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -9,13 +9,15 @@ import {
 
 import App from './App';
 import Page2 from './Page2'
+import Page3 from "./Page3";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
         <Routes>
-            <Route path="/" element={<App />} />
-            <Route path="/2" element={<Page2 />} />
+            <Route path="/" element={<App/>}/>
+            <Route path="/2" element={<Page2/>}/>
+            <Route path="/3" element={<Page3/>}/>
         </Routes>
     </BrowserRouter>
 );
